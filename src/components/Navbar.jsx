a9eaed24e4e7aeddd9a8ed97e883c23cf5da0502
amilpa <amilpa2020@gmail.com>
2023-08-19 14:31:09 +0530

    nav changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,23 @@ import Image from "next/image";
 
 import CustomButton from "./CustomButton.jsx";
 
-const NavBar = () => (
-  <header className='w-full  absolute z-10'>
-    <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
+const NavBar = ({ title }) => (
+  <header className="w-full  absolute z-10">
+    <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+      <Link href="/" className="flex justify-center items-center">
         <Image
-          src='/sage.png'
-          alt='logo'
+          src="/sage.png"
+          alt="logo"
           width={118}
           height={18}
-          className='object-contain'
+          className="object-contain"
         />
       </Link>
 
       <CustomButton
-        title='Sign in'
-        btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+        title={title}
+        btnType="button"
+        containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
       />
     </nav>
   </header>
