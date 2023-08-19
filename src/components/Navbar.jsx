5847fa48d48a57226449d9a8c0d8670ff68430b8
Abhinav <abhinavmohanan17@gmail.com>
2023-08-19 15:09:06 +0530

    Auth fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,37 @@
 "use client";
 import Link from "next/link";
-import { signIn } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 
 import CustomButton from "./CustomButton.jsx";
 
-const NavBar = ({ title }) => (
-  <header className="w-full  absolute z-10">
-    <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
-      <Link href="/" className="flex justify-center items-center">
-        <Image
-          src="/sage.png"
-          alt="logo"
-          width={118}
-          height={18}
-          className="object-contain"
-        />
-      </Link>
+const NavBar = () => {
+  const { data: session } = useSession();
 
-      <CustomButton
-        title={title}
-        btnType="button"
-        containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
-        handleClick={() => signIn("google")}
-      />
-    </nav>
-  </header>
-);
+  return (
+    <header className="w-full  absolute z-10">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+        <Link href="/" className="flex justify-center items-center">
+          <Image
+            src="/sage.png"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+        </Link>
+
+        <CustomButton
+          title={session && session.user ? "Sign Out" : "Sign In"}
+          btnType="button"
+          containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+          handleClick={() =>
+            session && session.user ? signOut() : signIn("google")
+          }
+        />
+      </nav>
+    </header>
+  );
+};
 
 export default NavBar;
