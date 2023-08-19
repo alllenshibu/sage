f53182653849db9c26092a8e1074ae80dd51e856
Allen Shibu <allenshibu@outlook.in>
2023-08-19 15:49:12 +0530

    quiz from database

--- src/app/api/quiz/attend/route.js
+++ src/app/api/quiz/attend/route.js
@@ -1,46 +1,27 @@
-
-
-const q = [
-    {
-        id: 1,
-        question: 'What is the capital of India?',
-        options: [
-            'New Delhi',
-            'Mumbai',
-            'Kolkata',
-            'Chennai',
-        ],
-        answer: 'New Delhi',
-    },
-    {
-        id: 2,
-        question: 'What is the capital of USA?',
-        options: [
-            'New York',
-            'Washington DC',
-            'Los Angeles',
-            'Chicago',
-        ],
-        answer: 'Washington DC',
-    }
-]
+import { pool } from '@/lib/pg'
 
 export async function GET(Request) {
 
     try {
         // Check if authenticated
+        
         // Database access
-        let quiz = [{}];
+        const { rows } = await pool.query(`SELECT * FROM quiz_question `)
 
-        q.forEach((qz) => {
-            quiz.push({
-                id: qz.id,
-                question: qz.question,
-                options: qz.options,
-            })
-        })
+        return new Response(JSON.stringify(rows), { status: 200 })
+    }
+    catch (err) {
+        return new Response(err.message, { status: 500 })
+    }
+}
 
-        return new Response(JSON.stringify(quiz), { status: 200 })
+export async function POST(Request) {
+
+    try {
+        // Check if authenticated
+        // Database access
+
+        return new Response(JSON.stringify(rows), { status: 200 })
     }
     catch (err) {
         return new Response(err.message, { status: 500 })
