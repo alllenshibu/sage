88da310056523d69c122ae6e135d70dd2b26f210
Abhinav <abhinavmohanan17@gmail.com>
2023-08-19 17:37:08 +0530

    changes

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,24 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+import GraphCard from "@/components/GraphCard";
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (session) return redirect("/");
+
+  return (
+    <div className="h-screen">
+      <div className="absolute top-[14%] left-[8%]">
+        <h1 className="text-3xl font-semibold">Hello Name</h1>
+        <h1 className="text-3xl font-semibold">
+          Welcome to your personal healthcare dashboard
+        </h1>
+      </div>
+      <GraphCard />
+
+      <div></div>
+    </div>
+  );
+}
