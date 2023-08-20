f432edca5d6727a3edc672c11cfb2eb4ff7b98c3
Anish Pillai <anishp8914@gmail.com>
2023-08-20 09:58:01 +0530

    Yoga

--- src/components/RelaxComponents/YogaCard.jsx
+++ src/components/RelaxComponents/YogaCard.jsx
@@ -11,13 +11,15 @@ import { Button, Stack, Typography } from '@mui/material';
 
 const YogaCard = ({yoga}) => {
   return (
-    <div className='w-300 h-455 bg-white border-t-2 rounded-bl-20 border-blue-500 flex justify-between flex-col pb-3 transition ease-in-out delay-150 bg-blue-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-100 duration-300'>
+    <Link href={`/relax/${yoga.id}`}>
+    <div className='w-300 h-455 bg-white border-t-2 rounded-bl-20 border-blue-500 flex justify-between flex-col pb-3 transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-indigo-100 duration-300'>
         <Image src={yoga.url_svg} alt={yoga.english_name} loading='lazy' height={200} width={200} className='p-3'/>
 
         <Typography ml="21px" color="#000" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize" className='sm:text-sm md:text-start flex flex-wrap'>
             {yoga.sanskrit_name_adapted}
         </Typography>
     </div>
+    </Link>
   )
 }
 
